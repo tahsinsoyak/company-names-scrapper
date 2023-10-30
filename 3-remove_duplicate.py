3a51d44b6446d6c984c80afc1b2028e354219a11
Tahsin Soyak <tahsinsoyakk@gmail.com>
2023-10-30 09:16:55 +0300

    Create 3-remove_duplicate.py

---
+++ 3-remove_duplicate.py
@@ -0,0 +1,19 @@
+# Open the input file for reading
+with open("filtered_skills.txt", "r") as file:
+    lines = file.readlines()
+
+# Remove duplicates while preserving order using a set
+unique_lines = []
+seen = set()
+
+for line in lines:
+    # Remove leading/trailing whitespace and convert to lowercase for case-insensitive comparison
+    cleaned_line = line.strip().lower()
+    
+    if cleaned_line not in seen:
+        seen.add(cleaned_line)
+        unique_lines.append(line)
+
+# Open the output file for writing
+with open("new_skills.txt", "w") as output_file:
+    output_file.writelines(unique_lines)
